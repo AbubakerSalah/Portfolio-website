1e2fb271474763b1d9e0d2397c038e898eefb835
Abubaker <137734278+AbubakerSalah@users.noreply.github.com>
2024-03-25 16:13:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const navToggle = document.querySelector(".nav-toggle")
-const navLinks = document.querySelectorAll(".nav-link")
 
-navToggle.addEventListener('click', () => {
-      document.body.classList.toggle('nav-open')
-})
-
-
-navLinks.forEach(link => {
-      link.addEventListener('click', () => {
-          document.body.classList.remove('nav-open');
-      })
-  })
-  
